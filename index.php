f8e764de4aaf5cda52ad4565040991297d78b961
Damien FONTAINE <dramaix2011@gmail.com>
2020-08-31 08:49:14 +0400

    test 1

--- index.php
+++ index.php
@@ -75,7 +75,7 @@
         <link rel="shortcut icon" type="image/png" href="favicon.ico"/>
 
         <!-- CSS
-        ================================================== -->       
+        =================================================== -->       
         
         <!-- Bootstrap css file-->
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
